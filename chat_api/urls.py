2e1c84298eabb7becd6d708149aefb8605ac9fe8
codewitgabi <gabrielmicheal497@gmail.com>
2022-12-13 06:10:41 +0100

    Fixed post request for api

--- chat_api/urls.py
+++ chat_api/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
 	path("<int:id>/", views.message_handler, name= "messages"),
 	path("send-message/<int:id>/", views.post_message_handler.as_view(), name= "post_messages"),
+	path("post/<int:id>/", views.post_message, name= "post"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- chat_api/views.py
+++ chat_api/views.py
@@ -25,7 +25,10 @@ def message_handler(request, id, format= None):
 		serializer = MessageSerializer(chat_messages, many= True)
 		
 		return Response(serializer.data, status= status.HTTP_200_OK)
-		
+
+
+"""
+Bug: cannot get id passed to url so getting the receiver is not possible
 
 class post_message_handler(generics.CreateAPIView):
 	lookup_field = "id"
@@ -37,3 +40,16 @@ class post_message_handler(generics.CreateAPIView):
 		sender = Chatter.objects.get(id= self.request.user.id)
 		receiver = Chatter.objects.get(id= 2)
 		serializer.save(sender= sender, receiver= receiver)
+"""
+		
+		
+@api_view(["POST"])
+def post_message(request, id):
+	if request.method == "POST":
+		serializer = MessageSerializer(data= request.data)
+		if serializer.is_valid():
+			sender = Chatter.objects.get(id= request.user.id)
+			receiver = Chatter.objects.get(id= id)
+			serializer.save(receiver= receiver, sender= sender)
+			return Response(serializer.data, status= status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
